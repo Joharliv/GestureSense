1cb4f55f7d5bd852eb65b15a5062125fc6db4820
Joharliv <joharliv@gmail.com>
2026-05-24 20:36:23 +0530

    added traceback logging

--- backend/app.py
+++ backend/app.py
@@ -304,7 +304,19 @@ def choose_suggestion():
 
 if __name__ == "__main__":
 
-   app.run(
-    host="0.0.0.0",
-    port=int(os.environ.get("PORT", 5000))
-)
+    try:
+
+        print("Starting GestureSense Backend...")
+
+        app.run(
+            host="0.0.0.0",
+            port=5000,
+            debug=True
+        )
+
+    except Exception as e:
+
+        import traceback
+
+        print("FULL ERROR:")
+        print(traceback.format_exc())
